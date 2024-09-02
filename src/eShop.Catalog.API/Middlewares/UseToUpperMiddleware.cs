5654898f1597ab553bdf01dcc35e0482f9880146
Nitin Londhe <nitin.londhe@genmills.com>
2024-09-02 22:16:35 +0530

    added a custom middleware attribute [UseToUpper]

---
+++ src/eShop.Catalog.API/Middlewares/UseToUpperMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using HotChocolate.Types.Descriptors;
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace eShop.Catalog.API.Middlewares;
+
+public static class UseToUpperObjectFieldDescriptorExtensions
+{
+    public static IObjectFieldDescriptor UseToUpper(this IObjectFieldDescriptor descriptor)
+    {
+        return descriptor.Use(next => async (context) =>
+        {
+            await next(context);
+
+            if (context.Result is string field)
+            {
+                context.Result = field.ToUpperInvariant();
+            }
+        });
+    }
+
+    public class UseToUpperAttribute : ObjectFieldDescriptorAttribute
+    {
+        public UseToUpperAttribute([CallerLineNumber] int order = default)
+        {
+            Order = order;
+        }
+
+        protected override void OnConfigure(
+            IDescriptorContext context,
+            IObjectFieldDescriptor descriptor,
+            MemberInfo member)
+        {
+            descriptor.UseToUpper();
+        }
+    }
+}
--- src/eShop.Catalog.API/Models/Brand.cs
+++ src/eShop.Catalog.API/Models/Brand.cs
@@ -1,5 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
+using static eShop.Catalog.API.Middlewares.UseToUpperObjectFieldDescriptorExtensions;
+
 namespace eShop.Catalog.API.Models;
 
 public sealed class Brand
@@ -7,6 +9,7 @@ public sealed class Brand
     public int Id { get; set; }
 
     [Required]
+    [UseToUpper]
     public string Name { get; set; } = default!;
 
     public ICollection<Product> Products { get; } = new List<Product>();
